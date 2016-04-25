773e4558ea678559d620e8d0600495614148c6a0
Bhavesh Sharma <bhaveshbs789@gmail.com>
2016-04-25 16:53:23 +0530

    Updated todo list

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'complete deveopment',
+	completed: false
+}, {
+	id: 2,
+	description: 'Goto new home',
+	completed: false
+}]
 
 app.get('/',function (req, res) {
 	res.send('Todo API Root');
