cd4263e5fc40c128a30be5eb91cae570527ea923
Bhavesh Sharma <bhaveshbs789@gmail.com>
2016-04-29 14:13:49 +0530

    Add /todos and /todos/:id

--- server.js
+++ server.js
@@ -9,10 +9,37 @@ var todos = [{
 	id: 2,
 	description: 'Goto new home',
 	completed: false
+}, {
+	id: 3,
+	description: 'Develop automata',
+	completed: true
 }]
 
 app.get('/',function (req, res) {
 	res.send('Todo API Root');
+})
+
+//GET /todos
+app.get('/todos', function (req, res) {
+	res.json(todos);
+})
+
+//GET /todos/:id
+app.get('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id);
+	var matchedTodo;
+	todos.forEach(function (todo) {
+		if (todoId === todo.id){
+			matchedTodo = todo;
+		}
+	});
+
+	if(matchedTodo){
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
+	//res.send("Asking for a todo item with id : " + req.params.id);
 })
 
 app.listen(PORT, function () {
