b01a7356f8e8beb3876d59505c6b1fcf5fe5d899
Bhavesh Sharma <bhaveshbs789@gmail.com>
2016-05-03 18:19:04 +0530

    Added DELETE for todo id

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -20,12 +21,13 @@ app.get('/todos', function (req, res) {
 //GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id);
-	var matchedTodo;
-	todos.forEach(function (todo) {
-		if (todoId === todo.id){
-			matchedTodo = todo;
-		}
-	});
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	//the underscore method above does the same as the below commented code ie 5 lines
+	// todos.forEach(function (todo) {
+	// 	if (todoId === todo.id){
+	// 		matchedTodo = todo;
+	// 	}
+	// });
 
 	if(matchedTodo){
 		res.json(matchedTodo);
@@ -36,13 +38,32 @@ app.get('/todos/:id', function (req, res) {
 })
 
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
+
 	body.id = todoNextId;
 	todoNextId++;
 
 	todos.push(body);
 	//console.log('Description : ' + body.description);
 	res.json(body);
+});
+
+app.delete('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if(!matchedTodo) {
+		res.status(404).json({"error": "no todo item found with the id"});
+	} else {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo)
+	}
 })
 
 app.listen(PORT, function () {
