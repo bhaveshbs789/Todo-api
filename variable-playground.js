de67f87d7419e0eb25b61baa7140935c9cf8ef1a
Bhavesh Sharma <bhaveshbs789@gmail.com>
2016-05-08 12:25:23 +0530

    Add variable-playground js file

---
+++ variable-playground.js
@@ -0,0 +1,28 @@
+// var person = {
+// 	name: 'Bhavesh',
+// 	age: '16'
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name : 'Sharma',
+// 	// 	age : 24
+// 	// };
+// 	obj.age = 24;
+// };
+
+// updatePerson(person);
+// console.log(person);
+
+//Arry Example
+
+var grades = [15, 24];
+
+function addGrade(grades) {
+	grades.push(30);
+	debugger;
+	//grades[0] = 90;
+}
+
+addGrade(grades);
+console.log(grades);
